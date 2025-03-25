dcbe2a1cd1741f9f6543300ca40862d7511d0fd0
aem2231 <mariojetullaji@gmail.com>
2025-03-25 13:37:33 +0000

    fixed death

--- src/player.py
+++ src/player.py
@@ -1,17 +1,21 @@
 import pygame
-from config import WIDTH, HEIGHT, GROUND_HEIGHT, GRAVITY, JUMP_STRENGTH
+import time
+from config import HEIGHT, GROUND_HEIGHT, GRAVITY, JUMP_STRENGTH
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, platforms_group, spikes_group):
         super().__init__()
         self.image = pygame.image.load("./assets/sprite.png").convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.x = WIDTH // 4
-        self.rect.y = HEIGHT - GROUND_HEIGHT - self.rect.height
+        self.start_x = 200
+        self.start_y = HEIGHT - GROUND_HEIGHT - 200 - self.rect.height
+        self.rect.x = self.start_x
+        self.rect.y = self.start_y
         self.vel_y = 0
         self.on_ground = False
         self.platforms = platforms_group
         self.spikes = spikes_group
+        self.reset()
 
     def moveRight(self, pixels):
         self.rect.x += pixels
@@ -38,9 +42,9 @@ class Sprite(pygame.sprite.Sprite):
         self.vel_y += GRAVITY
         self.rect.y += self.vel_y
         self.check_platform_collisions()
-        if self.check_spike_collision:
-            self.kill()
-            self.rect.y = 100
+        if self.check_spike_collision():
+            time.sleep(0.5)
+            self.reset()
         if self.rect.y >= HEIGHT - GROUND_HEIGHT - self.rect.height:
             self.rect.y = HEIGHT - GROUND_HEIGHT - self.rect.height
             self.vel_y = 0
@@ -48,4 +52,11 @@ class Sprite(pygame.sprite.Sprite):
 
     def check_spike_collision(self):
         for spike in self.spikes:
-            return self.rect.colliderect(spike.rect)
+            if self.rect.colliderect(spike.rect):
+                return True
+        return False
+
+    def reset(self):
+        self.rect.x = 200
+        self.rect.y = HEIGHT - GROUND_HEIGHT - 200 - self.rect.height
+        self.vel_y = 0
